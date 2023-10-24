1782e2e58d3aa7ed7f23f344231974ec4cc6d057
rollingsxshi <rollingsxshi@gmail.com>
2023-10-24 21:48:45 +0800

    get all tweets from db

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import Depends, FastAPI
+from models import Tweet
 import models
-from db import engine
+from db import SessionLocal, engine
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Depends - dependency injection
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(Tweet).all()
