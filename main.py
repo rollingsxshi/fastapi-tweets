2764d2780286209b06ade44fc1a0ea0ee7b1a624
rollingsxshi <rollingsxshi@gmail.com>
2023-10-24 22:08:01 +0800

    get tweet by id

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Path, status
 from models import Tweet
 import models
 from db import SessionLocal, engine
@@ -22,6 +22,17 @@ def get_db():
 # Depends - dependency injection
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.get("/")
+
+@app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Tweet).all()
+
+
+@app.get("/tweet/{tweet_id}", status_code=status.HTTP_200_OK)
+async def read_tweet(db: db_dependency, tweet_id: int = Path(gt=0)): # validate path param gt=0
+    tweet_model = db.query(Tweet).filter(Tweet.id == tweet_id).first()
+
+    if tweet_model is not None:
+        return tweet_model
+
+    raise HTTPException(status_code=404, detail='Tweet not found.')
