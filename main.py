ddc9e0cc17f9caa8356cabf6a8dad5f457531c11
rollingsxshi <rollingsxshi@gmail.com>
2023-10-28 00:14:51 +0800

    admin router & admin crud

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import models
 from db import engine
-from routers import auth, tweets
+from routers import auth, tweets, admin
 
 
 app = FastAPI()
@@ -10,3 +10,4 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(tweets.router)
+app.include_router(admin.router)
